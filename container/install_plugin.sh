55073aea4fb30181a7d24c322bd129abb4859bdb
killarny <killarny@gmail.com>
2016-04-14 02:11:19 -0600

    Delete install_plugin.sh

--- container/install_plugin.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash" > install_plugin.sh
-URL=$@
-UPDATE=$(curl -s $URL |python -c "import sys, bs4; url = bs4.BeautifulSoup(''.join([line for line in sys.stdin]), 'html5lib').find(class_='version_table').find('a', text='Download')['href']; print(url[url.rfind('=')+1:])")
-wget --trust-server-names "https://plugins.jetbrains.com/plugin/download?updateId=$UPDATE"
